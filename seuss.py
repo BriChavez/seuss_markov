8b98749a392dfc605d28a63fb4a23f7f760d565d
BriChavez <brianachavez@gmail.com>
2022-08-23 17:20:38 -0700

    oh she is a thing of beauty!

--- seuss.py
+++ seuss.py
@@ -3,7 +3,6 @@ import string
 from numpy.random import choice
 import random
 
-
 """READ FILE IN TO PLAY"""
 
 with open('extra_text.txt', 'r') as seuss_script:
@@ -12,8 +11,7 @@ with open('extra_text.txt', 'r') as seuss_script:
     seussical = seussical.lower()
 
 # use regex to split text into words and punctuation, including contracted words
-seuss_string = re.findall(r"(?:\w+[']?\w)+|[.,]", seussical)
-
+seuss_string = re.findall(r"[.,]|[a-z]+[']?[a-z]+", seussical)
 
 """CREATE OUR NESTED DICT TO SAVE HOW OFTEN ONE WORD FOLLOWS ANOTHER"""
 
@@ -21,7 +19,7 @@ seuss_dict = {}
 # for loop that runs over every word in our string, but stating to stop at the last word
 for i, word in enumerate(seuss_string[:-1]):
     # setting this word to be the word right after the one we were on
-    this_word = seuss_string[i-1]
+    this_word = seuss_string[i - 1]
     # if this_word isnt in our dictionary already...
     if this_word not in seuss_dict:
         # start our counter dict
@@ -41,7 +39,6 @@ for i, word in enumerate(seuss_string[:-1]):
         # lets add it and set its count to 1
         next_count[word] = 1
 
-
 """START THE STORY"""
 
 # starting out story off blank
@@ -57,10 +54,11 @@ if story == []:
     # set story up with our first word
     story.append(first_word)
 
-
 """CREATE THE WEIGHTED PROB ALGORITHM TO DETERMINE THE NEXT WORD"""
 
+
 # function to get the next word using weighted probability
+
 def whos_next():
     # set the current word to the last line of the story
     current_word = story[-1]
@@ -98,18 +96,19 @@ def whos_next():
             next_word = str(rng_says)
             # add our next word string to the story
             story.append(next_word)
-            
+
 
 """ADD WORDS TO OUR STORY, DROP WHITE SPACE, CAPITALIZE, AND NEWLINE"""
 
 # while our story is less than this long
-while len(story) < 600:
+while len(story) < 400:
     # call our function
     whos_next()
 # set our full story to be 
 whole_story = ' '.join(story)
 
 # replace white space preceding punctuation
+# Matches the contents of the group of the same number. Groups are numbered starting from 1
 whole_story = re.sub(r'\s([?.,!](?:\s|$))', r'\1', whole_story)
 
 # resplit the story, this time on sentences
@@ -117,15 +116,77 @@ sentences = re.split('[?.]', whole_story)
 # set a new clean variable
 cap_sent = []
 # run each sentence of our story through this for loop
+
+
+def cut_down(sentence):
+    """funtion to make our sentences have to be less than 13 words long"""
+    # if they arent...
+    if len(sentence.split(' ')) > 12:
+        # prefer to split on a comma, so lets do that first
+        if ',' in sentence:
+            # count the commas and pick the middle one to split it into two lines
+            *a, b = sentence.split(', ', sentence.count(',') // 2 + 1)
+            # the first half (a) is a list, so lets fix that, drop the leading white space, and capitalize the first word
+            a = ', '.join(a).lstrip().capitalize()
+            # capitalize the second half (b) and strip the leading white space
+            b = b.lstrip().capitalize()
+            # if the first half of the sentence is still too long,
+            if a.count(' ') > 12:
+                # we send it through this function
+                a = cut_down(a)
+            # same with b
+            if b.count(' ') > 12:
+                # cut it back if it is too long
+                b = cut_down(b)
+            # bring our new shorter sentences back together with a comma and a new line
+            sentence = ''.join(a) + ',\n' + b
+
+        # split the sentence if it has no commas
+        else:
+            # count the spaces and split in the middle
+            *a, b = sentence.split(' ', len(sentence.split(' ')) // 2 + 1)
+            # the first half (a) is a list, so lets fix that, drop the leading white space, and capitalize the first word
+            a = ' '.join(a).lstrip().capitalize()
+            # capitalize the second half (b) and strip the leading white space
+            b = b.lstrip().capitalize()
+            # if the first half of the sentence is still too long,
+            if a.count(' ') > 12:
+                # we send it through this function
+                a = cut_down(a)
+            # same with b
+            if b.count(' ') > 12:
+                # cut it back if it is too long
+                b = cut_down(b)
+            # bring our new shorter sentences back together with a comma and a new line
+            sentence = ''.join(a) + ',\n' + b
+
+        # now, lets get our sentence back
+        return sentence
+
+    # if the sentence isnt too long...
+    else:
+        # give it back to us
+        return sentence
+
+
+# lets clean up our story so the output looks prettier
 for sentence in sentences:
-    # strip the left white space, capitalize the first word of every sentence, add a period, and a new line
-    cap_sent.append((sentence.lstrip().capitalize() + '.' + "\n"))
-# turn our story back into a string by joining it back in together
-cap_sent = ' '.join(cap_sent)
+    # lets drop the leading white space and capitalize each sentence
+    sentence = sentence.lstrip().capitalize() + '.' + "\n"
+    # now, lets send each one through our function to make sure our sentences arent crazy long
+    sentence = cut_down(sentence)
+    # now that they look better, lets add it to a clean list
+    cap_sent.append(sentence)
+# join the list so it is a string
+cap_sent = ''.join(cap_sent)
 
+"""NOW LET'S SAVE AND READ OUR NEW TALE"""
 
-"""THE END. NOW LETS READ OUR NEW TALE"""
-# save our newly made and beautifully laid out dr seuss story
-text_file = open('saved_seuss/seuss_story2.txt', 'w')
+# open a file of whatever name we want to save it as
+text_file = open('saved_seuss/seuss_story5.txt', 'w')
+# write our newly made and beautifully laid out dr seuss story to the file
 text_file.write(cap_sent)
+# shut it down and share your fun story with a friend
 text_file.close()
+
+"""THE END"""
